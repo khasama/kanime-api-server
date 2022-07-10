cbd42fdd963978b92f42938c4f5896f1d1d9f1a2
khasama <khavl741953@gmail.com>
2022-07-10 12:16:43 +0700

    second deploy

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,6 +16,7 @@ UserController.loginAdminSite = (req, res , next) => {
                     .cookie('access_token', rs[1], {
                         maxAge: 365 * 24 * 60 * 60 * 100,
                         httpOnly: true,
+                        secure: true,
                         domain: 'kanime-server.herokuapp.com',
                     })
                     .json(rs[0]);
